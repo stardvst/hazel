ae9aa7d28334eb169267ab1666d537c265989c2d
Lilia <lilia.manukyan@hotmail.com>
2018-11-11 17:29:52 +0400

    Add loggin library, basic logging example

--- Hazel/Hazel/src/Hazel/EntryPoint.h
+++ Hazel/Hazel/src/Hazel/EntryPoint.h
@@ -6,6 +6,10 @@ extern Hazel::Application *Hazel::CreateApplication();
 
 int main(int argc, char *argv[])
 {
+	Hazel::Log::Init();
+	HZ_CORE_WARN("Initialized log");
+	HZ_INFO("Hello! Var={0}", 5);
+
 	auto app = Hazel::CreateApplication();
 	app->Run();
 	delete app;
